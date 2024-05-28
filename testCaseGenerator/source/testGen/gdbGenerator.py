f672fed070b76bc345f9eee6bdf44f024d595d29
gboud21 <gboud21@gmail.com>
2024-05-28 01:55:56 +0000

    Setup for GDB Generation

--- testCaseGenerator/source/testGen/gdbGenerator.py
+++ testCaseGenerator/source/testGen/gdbGenerator.py
@@ -1,3 +1,6 @@
+# Internal Imports
+from com.tst import testCase
+
 #######################################################################################################################
 ### This class generates a test script in the GDB Output Format
 class GdbGenerator:
@@ -6,5 +9,15 @@ class GdbGenerator:
     def __init__(self):
         pass
 
+    ###################################################################################################################
+    ### Generates the script to execute each of the test cases using GDB
     def generateGDBTestCases(self, testCaseList):
+        # Loop over the list of test cases generating each test case
+        for test in testCaseList:
+            self._generateTestCase(test)
+        pass
+    
+    ###################################################################################################################
+    ### Private method to generate an individual test case
+    def _generateTestCase(self, test: testCase.TestCase):
         pass
